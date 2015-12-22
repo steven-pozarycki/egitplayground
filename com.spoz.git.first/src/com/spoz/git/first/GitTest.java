a125f700bbf39ef75f39886ab35e2feb625d6ca2
steven-pozarycki <spoz@optonline.net>
2015-12-22 13:55:42 -0800

    Final Java update

--- com.spoz.git.first/src/com/spoz/git/first/GitTest.java
+++ com.spoz.git.first/src/com/spoz/git/first/GitTest.java
@@ -3,7 +3,7 @@ package com.spoz.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		 System.out.println("Git is cool or not ;)");
+		 System.out.println("Git is really cool!");
 	}
 
 }
