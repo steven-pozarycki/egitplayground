3f60089eacbed735277048b500081884cc88ace5
steven-pozarycki <spoz@optonline.net>
2015-12-22 19:54:17 -0800

    complete gittest code

--- com.spoz.git.first/src/com/spoz/git/first/GitTest.java
+++ com.spoz.git.first/src/com/spoz/git/first/GitTest.java
@@ -3,7 +3,7 @@ package com.spoz.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		 System.out.println("Git is really cool 999!");
+		 System.out.println("Git is really cool!!!");
 	}
 
 }
